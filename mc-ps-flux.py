f0c60d7c1a8a35e9f1ae5d579ce1eaca250095f4
kunok-chang <kunok.chang@khu.ac.kr>
2025-04-15 10:28:25 -0700

    Update mc-ps-flux.py

--- mc-ps-flux.py
+++ mc-ps-flux.py
@@ -14,22 +14,9 @@ def monte_carlo_photon_flux(S_p, distance, num_samples=1000000):
     """
     # Cross-sectional area of a sphere at distance r
     cross_sectional_area = 4 * np.pi * distance**2
-    
-    # Generate random angles for photon emission
-    theta = np.arccos(1 - 2 * np.random.rand(num_samples))  # Polar angle (0 to π)
-    phi = 2 * np.pi * np.random.rand(num_samples)           # Azimuthal angle (0 to 2π)
-
-    # Direction of photons (normalized)
-    x_dir = np.sin(theta) * np.cos(phi)
-    y_dir = np.sin(theta) * np.sin(phi)
-    z_dir = np.cos(theta)
-
-    # Test photons' passage through a spherical shell at distance r
-    # All photons originate from the source (0, 0, 0) and travel outward
-    photons_through_area = num_samples  # All photons are emitted isotropically
 
     # Photon flux
-    flux = (photons_through_area / num_samples) * (S_p / cross_sectional_area)
+    flux = S_p / cross_sectional_area
     return flux
 
 # Parameters
